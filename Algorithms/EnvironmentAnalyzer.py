5f9ef587fbfb6c2599728323350c5253a483814e
Alexaline Thomas <thomas.alexaline@etu.u-bordeaux.fr>
2020-06-01 11:26:02 +0200

    log update + finally, here is the proper tree deletion in the lot

--- Algorithms/EnvironmentAnalyzer.py
+++ Algorithms/EnvironmentAnalyzer.py
@@ -3,34 +3,17 @@ import math
 import RNG
 import logging
 import utilityFunctions as utilityFunctions
+import BlocksInfo as BlocksInfo
 from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox, level
 
-WOOD_ID = [17, 162]
 GROUND_ID = [1, 2, 3, 12, 13, 16, 79, 80, 82, 110, 159, 174]
 PLANT_ID = [31, 32, 81, 78, 99, 100, 106, 111, 127]
-NO_SUB_ID = [99, 100, 106] #block with useless sub_id info
+NO_SUB_ID = [99, 100, 106, 127] #block with useless sub_id info
 FLUID_ID = [9]
 
-found_wood = {'17:0': 0, '17:1': 0, '17:2': 0, '17:3': 0, '162:0': 0, '162:1': 0}
+found_wood = {(17, 0): 0, (17, 1): 0, (17, 2): 0, (17, 3): 0, (162, 0): 0, (162, 1): 0}
 found_blocks = {}
 found_plants = {}
-
-BADLANDS_TERRACOTTA_ID = ['159:0', '159:1', '159:4', '159:8', '159:12', '159:14', '172:0']
-MYCELIUM_ID = '110:0'
-LILY_PAD_ID = '111:0'
-SAND_ID = '12:0'
-RED_SAND_ID = '12:1'
-DEAD_BUSH_ID = '31:0'
-CACTUS_ID = '81:0'
-COAL_ORE_ID = '16:0'
-COCOA_ID = '127:0'
-VINES_ID = '106'
-RED_MUSHROOM_ID = '100'
-BROWN_MUSHROOM_ID = '99'
-SNOW_ID = '78:0'
-STONE_ID = '1:0'
-GRAVEL_ID = '13:0'
-ICE_ID = '79:0'
 
 def determinate_usable_wood(level, height_map, x_min, x_max, z_min, z_max) :
     usable_wood = {}
@@ -42,23 +25,23 @@ def determinate_usable_wood(level, height_map, x_min, x_max, z_min, z_max) :
         for x in range(x_min, x_max):
             y = height_map[x - x_min][z - z_min]
             block = level.blockAt(x, y, z)
-            while not block in GROUND_ID :
-                if block in WOOD_ID :
+            while not block in GROUND_ID and not block in FLUID_ID :
+                if block in BlocksInfo.LOGS_ID :
                     sub_id = level.blockDataAt(x, y, z)
                     if (block == 17 and sub_id > 3) or (block == 162 and sub_id > 1) :
                         sub_id = 0
-                    found_wood[str(block) + ':' + str(sub_id)] += 1
+                    found_wood[(block, sub_id)] += 1
                     total_found += 1
                 elif block in PLANT_ID :
                     if block in NO_SUB_ID :
-                        block_id = str(block)
+                        block_id = block
                     else :
-                        block_id = str(block) + ':' + str(level.blockDataAt(x, y, z))
+                        block_id = (block, level.blockDataAt(x, y, z))
                     addToDictionary(found_plants, block_id)
                 y = y - 1
                 block = level.blockAt(x, y, z)
             if block in GROUND_ID :
-                block_id = str(level.blockAt(x, y, z)) + ':' + str(level.blockDataAt(x, y, z))
+                block_id = (level.blockAt(x, y, z), level.blockDataAt(x, y, z))
                 addToDictionary(found_blocks, block_id)
                 total_ground += 1
             elif block in FLUID_ID :
@@ -66,7 +49,7 @@ def determinate_usable_wood(level, height_map, x_min, x_max, z_min, z_max) :
                 total_ground += 1
 
     logging.info("---------------------------------------------")
-    logging.info("found wood : oak : {}, spruce {}, birch {}, jungle {}, acacia {}, dark_oak {}".format(found_wood['17:0'], found_wood['17:1'], found_wood['17:2'], found_wood['17:3'], found_wood['162:0'], found_wood['162:1']))
+    logging.info("found wood : oak : {}, spruce {}, birch {}, jungle {}, acacia {}, dark_oak {}".format(found_wood[(17, 0)], found_wood[(17, 1)], found_wood[(17, 2)], found_wood[(17, 3)], found_wood[(162, 0)], found_wood[(162, 1)]))
     logging.info("---------------------------------------------")
     logging.info("block found : {}".format(found_blocks))
     logging.info("---------------------------------------------")
@@ -80,7 +63,7 @@ def determinate_usable_wood(level, height_map, x_min, x_max, z_min, z_max) :
                 usable_wood[key] = percentage
     else :
         """ Todo add a behavior if no wood """
-        usable_wood['17:0'] = 100
+        usable_wood[(17, 0)] = 100
 
     biome = identifyBiome(total_ground, total_water, total_found)
 
@@ -91,66 +74,66 @@ def determinate_usable_wood(level, height_map, x_min, x_max, z_min, z_max) :
 def identifyBiome(total_ground, total_water, total_found):
     terracotta_count = badlandsTerracottaCount()
     logging.info("Water percentage = {}".format(total_water / float(total_ground)))
-    if found_blocks.has_key(SAND_ID) or found_blocks.has_key(RED_SAND_ID) or terracotta_count > 0:
-        red_sand = (found_blocks[RED_SAND_ID] if found_blocks.has_key(RED_SAND_ID) else 0) + terracotta_count
-        sand =  found_blocks[SAND_ID] if found_blocks.has_key(SAND_ID) else 0
+    if found_blocks.has_key(BlocksInfo.SAND_ID) or found_blocks.has_key(BlocksInfo.RED_SAND_ID) or terracotta_count > 0:
+        red_sand = (found_blocks[BlocksInfo.RED_SAND_ID] if found_blocks.has_key(BlocksInfo.RED_SAND_ID) else 0) + terracotta_count
+        sand =  found_blocks[BlocksInfo.SAND_ID] if found_blocks.has_key(BlocksInfo.SAND_ID) else 0
         #logging.info("Sand percentage = {}".format((red_sand + sand) / float(total_ground)))
         if (red_sand + sand) / float(total_ground) > 0.45 :
             ##logging.info("red Sand {} and Sand {}".format(red_sand, sand))
             if red_sand > sand :
                 return 'Badlands'
             else :
-                if found_blocks.has_key(DEAD_BUSH_ID) or found_blocks.has_key(CACTUS_ID) :
+                if found_plants.has_key(BlocksInfo.DEAD_BUSH_ID) or found_plants.has_key((BlocksInfo.CACTUS_ID, 0)) :
                     return 'Desert'
                 elif total_water / float(total_ground) > 0.3 :
                     return 'Beach'
                 else :
                     return 'Desert'
-    if found_plants.has_key(SNOW_ID) :
-        if found_blocks.has_key(STONE_ID) :
-            if found_blocks[STONE_ID] / float(total_ground) > 0.2 :
-                logging.info("Stone percentage = {}".format(found_blocks[STONE_ID] / float(total_ground)))
+    if found_plants.has_key(BlocksInfo.SNOW_ID) :
+        if found_blocks.has_key(BlocksInfo.STONE_ID) :
+            if found_blocks[BlocksInfo.STONE_ID] / float(total_ground) > 0.22 :
+                logging.info("Stone percentage = {}".format(found_blocks[BlocksInfo.STONE_ID] / float(total_ground)))
                 return 'Mountains'
-        if found_blocks.has_key(GRAVEL_ID) :
-            if found_blocks[GRAVEL_ID] / float(total_ground) > 0.2 :
+        if found_blocks.has_key(BlocksInfo.GRAVEL_ID) :
+            if found_blocks[BlocksInfo.GRAVEL_ID] / float(total_ground) > 0.22 :
                 return 'Mountains'
         return 'Snowy_zone'
-    elif found_blocks.has_key(MYCELIUM_ID) :
-        if found_blocks[MYCELIUM_ID] / float(total_ground) > 0.45 :
+    elif found_blocks.has_key(BlocksInfo.MYCELIUM_ID) :
+        if found_blocks[BlocksInfo.MYCELIUM_ID] / float(total_ground) > 0.45 :
             return 'Mushroom Fields'
     if total_found > 0 :
-        if float(found_wood['162:1']) / total_found > 0.5:
+        if float(found_wood[(162, 1)]) / total_found > 0.5:
             return 'Dark_forest'
-        elif float(found_wood['17:3']) / total_found > 0.5 :
+        elif float(found_wood[(17, 3)]) / total_found > 0.5 :
             return 'Jungle'
-        elif float(found_wood['162:0']) / total_found > 0.3 :
+        elif float(found_wood[(162, 0)]) / total_found > 0.3 :
             return 'Savanna'
-        elif float(found_wood['17:0'] + found_wood['17:1']) / total_found > 0.6 and total_water / float(total_ground) < 0.2 :
+        elif float(found_wood[(17, 0)] + found_wood[(17, 2)] + found_wood[(17, 1)]) / total_found > 0.6 and total_water / float(total_ground) < 0.2 :
             logging.info("base with wood")
             return 'Base'
     if total_water / float(total_ground) > 0.7 :
         return 'Ocean'
-    elif found_blocks.has_key(STONE_ID) :
-        if found_blocks[STONE_ID] / float(total_ground) > 0.3 :
+    elif found_blocks.has_key(BlocksInfo.STONE_ID) :
+        if found_blocks[BlocksInfo.STONE_ID] / float(total_ground) > 0.25 :
             return 'Mountains'
-    if found_blocks.has_key(ICE_ID) :
-        if found_blocks[ICE_ID] / float(total_water) > 0.5 :
+    if found_blocks.has_key(BlocksInfo.ICE_ID) :
+        if found_blocks[BlocksInfo.ICE_ID] / float(total_water) > 0.5 :
             return 'Snowy_zone'
     #This part is just in order to return something
-    if found_plants.has_key(VINES_ID) :
-        if found_plants.has_key(LILY_PAD_ID) or found_plants.has_key(RED_MUSHROOM_ID) or found_plants.has_key(BROWN_MUSHROOM_ID):
+    if found_plants.has_key(BlocksInfo.VINES_ID) :
+        if found_plants.has_key(BlocksInfo.LILY_PAD_ID) or found_plants.has_key(BlocksInfo.RED_MUSHROOM_ID) or found_plants.has_key(BlocksInfo.BROWN_MUSHROOM_ID):
             return 'Swamp'
-        elif found_plants.has_key(COCOA_ID) :
+        elif found_plants.has_key(BlocksInfo.COCOA_ID) :
             return 'Jungle'
-    elif found_blocks.has_key(COAL_ORE_ID) :
+    elif found_blocks.has_key(BlocksInfo.COAL_ORE_ID) :
             return 'Mountains'
     elif found_blocks.has_key(MYCELIUM_ID) :
         return 'Mushroom_fields'
-    elif found_plants.has_key(LILY_PAD_ID) :
+    elif found_plants.has_key(BlocksInfo.LILY_PAD_ID) :
         return 'Swamp'
-    elif found_plants.has_key(COCOA_ID):
+    elif found_plants.has_key(BlocksInfo.COCOA_ID):
         return 'Jungle'
-    elif (found_plants.has_key(RED_MUSHROOM_ID) or found_plants.has_key(BROWN_MUSHROOM_ID)) and found_wood['162:1'] > 0 :
+    elif (found_plants.has_key(BlocksInfo.RED_MUSHROOM_ID) or found_plants.has_key(BlocksInfo.BROWN_MUSHROOM_ID)) and found_wood[(162, 1)] > 0 :
         return 'Dark_forest'
     return 'Base'
 
@@ -162,6 +145,6 @@ def addToDictionary(dictionary, key):
 
 def badlandsTerracottaCount():
     terracotta_count = 0
-    for i in range(0, len(BADLANDS_TERRACOTTA_ID)):
-        terracotta_count += found_blocks[BADLANDS_TERRACOTTA_ID[i]] if found_blocks.has_key(BADLANDS_TERRACOTTA_ID[i]) else 0
+    for i in range(0, len(BlocksInfo.BADLANDS_TERRACOTTA_ID)):
+        terracotta_count += found_blocks[BlocksInfo.BADLANDS_TERRACOTTA_ID[i]] if found_blocks.has_key(BlocksInfo.BADLANDS_TERRACOTTA_ID[i]) else 0
     return terracotta_count
